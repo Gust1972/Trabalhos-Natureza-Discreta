3aab2e24a913cce41c03451c252157a8bb4a8433
GustavoEdS <101608437+Gust1972@users.noreply.github.com>
2022-08-11 17:50:37 -0300

    Update main.py

--- Trabalho conjuntos 1/main.py
+++ Trabalho conjuntos 1/main.py
@@ -4,22 +4,33 @@ print(linhas)
 
 def fazerConjunto(linha):
     conjunto = linha.split(", ")
-    conjunto = set([int(i) for i in conjunto])
     return conjunto
 
 def fazerConjuntostr(linha):
     conjunto = linha.split(", ")
-    conjunto = set([str(i) for i in conjunto])
     return conjunto
   
 def fazerUniao(c1, c2):
-    return c1.union(c2)
+    list = []
+    list.extend(c1)
+    for x in c2:
+      if x not in list:
+        list.append(x)
+    return list
 
 def fazerInter(co1, co2):
-    return co1.intersection(co2)
+    list = []
+    for x in co1:
+      if x in co2:
+        list.append(x)
+    return list
 
 def fazerDif(con1, con2):
-    return con1.diference(con2)
+    list = []
+    for x in con1:
+      if x not in con2:
+        list.append(x)
+    return list
 
 def fazerProd(con1):
   resultado = [[]]
@@ -42,7 +53,7 @@ for i, linha in enumerate(linhas):
   elif linha[0] == "D":
     c7 = fazerConjuntostr(linhas[i+1])
     c8 = fazerConjuntostr(linhas[i+2])
-    print('Diferença: Conjunto 1',c7,"conjunto 2",c8,"resultado:",c7.difference(c8))
+    print('Diferença: Conjunto 1',c7,"conjunto 2",c8,"resultado:",fazerDif(c7,c8))
   elif linha[0] == "C":
     c9 = linhas[i+1].split(", ")
     c10 = linhas[i+2].split(", ")
@@ -50,4 +61,3 @@ for i, linha in enumerate(linhas):
     conjuntosProd.append(c9)
     conjuntosProd.append(c10)
     print("Produto Cartesiano: Conjunto1",c9,"Conjunto 2", c10,"Resultado:", fazerProd(conjuntosProd))
-      
